721a1ebee2eea6099a65d7fa3b9d703957564669
Nishith Modi <nishith.modi@searce.com>
2014-12-16 11:39:15 +0530

    readme updated

--- README.md
+++ README.md
@@ -2,3 +2,43 @@ AngularAlertFlash
 =================
 
 Alert plugin for angular application
+
+Usage
+-----
+
+Creats stack of alerts based on calls. It provides one service(configurable) and directive.
+
+Include alert.min.js and alert.min.css in html and declare 'alert-flash' directive within scope of your application.
+See below for example:
+
+```html 
+  <head>
+    <link href="dist/alert.min.css" rel="stylesheet">
+  </head>
+  <body ng-app="myApp">
+    <alert-flash></alert-flash>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.9/angular.js"></script>
+    <script src="bower_componentns/AngularAlertFlash/dist/alert.min.js"></script>
+  </body>
+```
+
+Inject 'alert' module at application level module. Use provider to configure elapsed time for alerts. Default is 5sec.
+See below for example:
+
+```js
+angular.module('myApp',['alert']).config(function(alertServiceProvider){
+    alertServiceProvider.setAlertTime(millis);
+});
+```
+
+Inject 'alertService' in controller to alert.
+
+```js
+angular.controller('myController', function(alertService){
+    alertService.success(message);
+    alertService.error(message);
+    alertService.info(message)
+});
+```
+
+To enable animation of flashing alert, include ngAnimate.
--- app/scripts/alert.js
+++ app/scripts/alert.js
@@ -2,29 +2,7 @@
  * Created by nishith.modi on 24/11/14.
  */
 
-/**
- * @ngDoc
- * @author nishith modi
- * @name alert module
- * @module
- *
- * @description creats stack of alerts based on calls.
- * It provides one service(configurable) and directive
- * alert.js and alert.css file is required to use this module in your application
- *
- * How to use it:
- * inject module 'alert' at your application root.
- * Use alertServiceProvider to config the following:
- *      (1) alertServiceProvider.setAlertTime(millis) : Up to what millisecond time, alert is to be shown
- *
- * <alert-flash></alert-flash> - use this element in your body and within the scope of angular
- * inject 'alertService' in controller and use the following methods:
- *       (1) alertService.success(message) - success message to be shown
- *       (2) alertService.error(message) - error message to be shown
- *       (3) alertService.info(message) - info message to be shown
- *
- * Animation is also added, will work if ngAnimate is included
- */
+'use strict';
 angular.module('alert', [])
     .constant('alertConfig', {
         success: 'alert-success',
@@ -57,12 +35,12 @@ angular.module('alert', [])
                         var alertPack = {
                             typeOfAlert: alertConfig[type],
                             msg: msg
-                        }
+                        };
 
                         message.push(alertPack);
                         this.hideAlert(alertPack);
                     },
-                    hideAlert: function (which) {
+                    hideAlert: function () {
                         $timeout(function () {
                             message.shift();
                         }, flashTime);
@@ -76,7 +54,7 @@ angular.module('alert', [])
             restrict: 'E',
             template: "<div class='alert-container'><div class='repeat-animation' ng-repeat='alert in alerts'><div class='alert' ng-class='alert.typeOfAlert' ng-bind='alert.msg'></div></div></div>",
             scope: {},
-            link: function($scope, $element, $attrs) {
+            link: function($scope) {
                 $scope.$watch(alertService.getAlert, function(){
                     $scope.alerts = alertService.getAlert();
                 });
